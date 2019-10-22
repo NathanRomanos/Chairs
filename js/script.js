abab66bc42a09d647e02222335f5bddc02210217
Nathan michael glenn Romanos <932400754@bun2-206-07.local>
2019-10-22 15:22:54 +1300

    first commit

---
+++ js/script.js
@@ -0,0 +1,139 @@
+console.log('working');
+
+var chairs = [
+  {
+    name : 'Ekedalen Chair',
+    legs : 4,
+    people : 1,
+    material : 'wood and fabric',
+    size : 'Small',
+    reclinable : false,
+    photo : 'images/ekedalen.jpg'
+  },
+  {
+    name : 'Small Patio Chair',
+    legs : 4,
+    people : 1,
+    material : 'fabric, yarn, and wood',
+    size : 'Small',
+    reclinable : false,
+    photo : 'images/patio.jpg'
+  },
+  {
+    name : 'Accent Chair',
+    legs : 4,
+    people : 1.5,
+    material : 'fabric, leather, and wood',
+    size : 'Medium',
+    reclinable : false,
+    photo : 'images/accent.jpg'
+  },
+  {
+    name : 'Sun Lounger',
+    legs : 4,
+    people : 1,
+    material : 'fabric, metal, and plastic',
+    size : 'Large',
+    reclinable : true,
+    photo : 'images/sunLounger.jpg'
+  },
+  {
+    name : 'Lazy Boy',
+    legs : 4,
+    people : 1.5,
+    material : 'fabric, leather, and wood',
+    size : 'Large',
+    reclinable : true,
+    photo : 'images/lazyBoy.jpg'
+  },
+  {
+    name : 'Stool',
+    legs : 4,
+    people : 1,
+    material : 'wood',
+    size : 'Small',
+    reclinable : false,
+    photo : 'images/stool.jpg'
+  },
+  {
+    name : 'Office Chair',
+    legs : 5,
+    people : 1,
+    material : 'plastic, metal, and fabric',
+    size : 'Small',
+    reclinable : true,
+    photo : 'images/office.jpg'
+  },
+  {
+    name : 'Throne',
+    legs : 4,
+    people : 1,
+    material : 'metal, fabric, and wood',
+    size : 'Medium',
+    reclinable : false,
+    photo : 'images/throne.jpg'
+  },
+  {
+    name : 'Bar Stool',
+    legs : 1,
+    people : 1,
+    material : 'metal and fabric',
+    size : 'Small',
+    reclinable : false,
+    photo : 'images/barStool.jpg'
+  },
+  {
+    name : ,
+    legs : ,
+    people : ,
+    material : ,
+    size : ,
+    reclinable : ,
+    photo :
+  },
+  {
+    name : ,
+    legs : ,
+    people : ,
+    material : ,
+    size : ,
+    reclinable : ,
+    photo :
+  },
+  {
+    name : ,
+    legs : ,
+    people : ,
+    material : ,
+    size : ,
+    reclinable : ,
+    photo :
+  },
+  {
+    name : ,
+    legs : ,
+    people : ,
+    material : ,
+    size : ,
+    reclinable : ,
+    photo :
+  },
+  {
+    name : ,
+    legs : ,
+    people : ,
+    material : ,
+    size : ,
+    reclinable : ,
+    photo :
+  },
+  {
+    name : ,
+    legs : ,
+    people : ,
+    material : ,
+    size : ,
+    reclinable : ,
+    photo :
+  },
+]
